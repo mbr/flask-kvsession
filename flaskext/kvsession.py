5a5906a36af3c1744d027c03bf4e6318206ca969
Marc Brinkmann <git@marcbrinkmann.de>
2011-07-04 14:56:51 +0200

    Initial creation through make-flaskext.py.

---
+++ flaskext/kvsession.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.kvsession
+    ~~~~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2011 by Marc Brinkmann.
+    :license: MIT, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Flask-KVSession
+---------------
+
+Description goes here...
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-KVSession>`_
+* `development version
+  <http://github.com/USERNAME/REPOSITORY/zipball/master#egg=Flask-KVSession-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-KVSession',
+    version='0.1',
+    url='<enter URL here>',
+    license='MIT',
+    author='Marc Brinkmann',
+    author_email='your-email-here@example.com',
+    description='<enter short description here>',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
