8aae3da3b708f7e08959ea3d9406398baa55479b
Marc Brinkmann <git@marcbrinkmann.de>
2011-07-05 15:38:04 +0200

    Partially complete implementation.
    Still missing tests are verifying that values are saved and restored
    correctly and manipulations of any kind are detected.

--- flaskext/kvsession.py
+++ flaskext/kvsession.py
@@ -11,7 +11,10 @@
     :license: MIT, see LICENSE for more details.
 """
 
-from flask.session import Session
+import hmac
+
+from flask import current_app
+import flask
 from random import SystemRandom
 import time
 
@@ -29,35 +32,61 @@ def generate_session_key(expires=None, bits=64):
     return '%x_%x' % (idbits, expires)
 
 
-def open_session(self, request):
-    key = self.secret_key
-    if key is not None:
-        pass #  create or load session instance here
-
-
-class KVSession(Session):
+class Session(flask.Session):
     def serialize(self, expires=None):
         # get session serialization
-        sdata = super(KVSession, self).serialize(expires)
+        sdata = super(Session, self).serialize(expires)
 
-        # store sdata, receive key
-        # only exceptions expected are ValueErrors, which should never happen
-        # with proper key generation and IOErrors, which we do not want to
-        # catch here
-        key = self.store.put(data=sdata)
+        # store sdata, receive key. the only exceptions expected are
+        # ValueErrors, which should never happen with proper key generation
+        # and IOErrors, which we do not want to catch here
+        key = current_app.session_kvstore.put(
+            key=generate_session_key(
+                expires,
+                current_app.config['SESSION_KEY_BITS']
+            ),
+            data=sdata
+        )
 
-        return key
+        # sign key using HMAC to make guessing impossible
+        mac = hmac.new(self.secret_key, key, self.hash_method)
+
+        return '%s_%s' % (key, mac.hexdigest())
 
     @classmethod
-    def unserialize(self, string, secret_key):
-        key = string
+    def unserialize(cls, string, secret_key):
+        key, mac_hexdigest = string.rsplit('_',1)
 
-        # load sdata from store
-        try:
-            string = self.store.get(key)
-        except KeyError:
-            # someone deleted the session
-            pass
+        sdata = ''
+
+        mac = hmac.new(secret_key, key, cls.hash_method)
+
+        if mac.hexdigest() == mac_hexdigest:
+            # mac okay, load sdata from store
+            try:
+                sdata = self.store.get(key)
+            except KeyError:
+                # someone deleted the session, leave sdata as ''
+                pass
 
         # unserialize "normally"
-        return super(KVSession, self).unserialize(string, secret_key)
+        return super(Session, cls).unserialize(string, secret_key)
+
+
+# the actual extension class
+class KVSession(object):
+    def __init__(self, session_kvstore, app=None):
+        app.session_kvstore = session_kvstore
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        self.app.config.setdefault('SESSION_KEY_BITS', 64)
+        self.app.open_session = self.open_session
+
+    def open_session(self, request):
+        key = self.app.secret_key
+        if key is not None:
+            return Session.load_cookie(request, self.app.session_cookie_name,
+                secret_key=key)
--- tests/test_flask_kvsession.py
+++ tests/test_flask_kvsession.py
@@ -10,7 +10,9 @@ else:
 
 import time
 
-from flaskext.kvsession import generate_session_key
+from simplekv.memory import DictStore
+from flask import Flask, session
+from flaskext.kvsession import generate_session_key, KVSession
 
 
 class TestGenerateSessionKey(unittest.TestCase):
@@ -42,3 +44,49 @@ class TestGenerateSessionKey(unittest.TestCase):
 
             self.assertNotIn(id, ids)
             ids.add(id)
+
+
+def create_app(store):
+    app = Flask(__name__)
+
+    KVSession(store, app)
+
+    @app.route('/')
+    def index():
+        return 'nothing to see here, move along'
+
+    @app.route('/store-in-session/<key>/<value>/')
+    def store(key, value):
+        session[key] = value
+        return 'stored %r at %r' % (value, key)
+
+    return app
+
+
+class TestSampleApp(unittest.TestCase):
+    def setUp(self):
+        self.store = DictStore()
+        self.app = create_app(self.store)
+        self.app.config['TESTING'] = True
+        self.app.config['SECRET_KEY'] = 'devkey'
+
+        self.client = self.app.test_client()
+
+    def test_app_setup(self):
+        pass
+
+    def test_app_request_no_extras(self):
+        rv = self.client.get('/')
+
+        self.assertIn('move along', rv.data)
+
+    def test_no_session_usage_uses_no_storage(self):
+        rv = self.client.get('/')
+        rv2 = self.client.get('/')
+
+        self.assertEqual({}, self.store.d)
+
+    def test_session_usage(self):
+        self.client.get('/store-in-session/foo/bar/')
+
+        self.assertNotEqual({}, self.store.d)
