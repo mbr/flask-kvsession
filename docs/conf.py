401694b15e8f68bf5dc27f94b46ec83ee25b3aee
Marc Brinkmann <git@marcbrinkmann.de>
2011-07-11 12:27:50 +0200

    Fixed syntax error in docs conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Flask-KVsession' copyright = u'2011, Marc Brinkmann'
+project = u'Flask-KVsession'
+copyright = u'2011, Marc Brinkmann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
