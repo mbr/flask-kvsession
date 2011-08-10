eecf2b00df299ece292bdd54b350faaca49f9b5c
Marc Brinkmann <git@marcbrinkmann.de>
2011-08-10 15:29:21 +0200

    Next version will be 0.3.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name='Flask-KVSession',
-    version='0.2dev',
+    version='0.3dev',
     url='https://github.com/mbr',
     license='MIT',
     author='Marc Brinkmann',
