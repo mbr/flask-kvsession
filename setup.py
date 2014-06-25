42b7f70cdee3d21767b689c3ccad3343b728096e
Marc Brinkmann <git@marcbrinkmann.de>
2014-06-25 17:53:38 +0200

    Move from module to package.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email='git@marcbrinkmann.de',
     description='Transparent server-side session support for flask',
     long_description=read('README.rst'),
-    py_modules=['flask_kvsession'],
+    packages=['flask_kvsession'],
     zip_safe=False,
     platforms='any',
     install_requires=[
