8e9e28a97fda81f2b127790bf45d8f2217fcaeef
Marc Brinkmann <git@marcbrinkmann.de>
2015-05-07 09:09:24 +0200

    Additionally test against original behaviour. Closes #27.

--- tests/test_github_issues.py
+++ tests/test_github_issues.py
@@ -1,17 +1,21 @@
 from flask import Flask, session
 from flask_kvsession import KVSessionExtension
-from simplekv.memory.redisstore import RedisStore
+import pytest
 import redis
+from simplekv.memory.redisstore import RedisStore
 
 
-# contains regression tests against various reported issues
-
-def test_issue_27():
+@pytest.fixture(
+    # True: KVSession enabled, otherwise use flasks sessions
+    params=[False, True]
+)
+def issue27app(request):
     app = Flask(__name__)
     app.secret_key = 'foo'
 
     # toggle for default session
-    KVSessionExtension(RedisStore(redis.StrictRedis()), app)
+    if request.param is True:
+        KVSessionExtension(RedisStore(redis.StrictRedis()), app)
 
     @app.route('/')
     def index():
@@ -24,9 +28,15 @@ def test_issue_27():
         session['x'] = session.get('x', 0) + 1
         return str(session['x'])
 
+    return app
+
+
+# contains regression tests against various reported issues
+def test_issue_27(issue27app):
+
     # client:
     expected = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5']
-    with app.test_client() as c:
+    with issue27app.test_client() as c:
         for i, e in zip(range(10), expected):
             resp = c.get('/' + (i % 2 == 0 and 'test' or ''))
 
